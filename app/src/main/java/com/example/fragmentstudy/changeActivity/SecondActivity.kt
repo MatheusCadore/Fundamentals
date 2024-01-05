bcca724a6e96500a72fc5d6afe20689340d7a12f
matheus <matheusecadore@gmail.com>
2024-01-05 01:50:41 -0300

    Layout do SingUp pronto, começo da logica feito.

--- app/src/main/java/com/example/fragmentstudy/changeActivity/SecondActivity.kt
+++ app/src/main/java/com/example/fragmentstudy/changeActivity/SecondActivity.kt
@@ -1,12 +1,43 @@
 package com.example.fragmentstudy.changeActivity
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.example.fragmentstudy.R
+import com.example.fragmentstudy.changeFrag.MainActivity
+import com.example.fragmentstudy.databinding.ActivityMainBinding
+import com.example.fragmentstudy.databinding.ActivitySecondBinding
 
 class SecondActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivitySecondBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_second)
+        binding = ActivitySecondBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        goBack()
+        saveUserData()
+
     }
+
+    private fun goBack(){
+        binding.btnBack.setOnClickListener {
+            Intent(this, MainActivity::class.java).also {
+                startActivity(it)
+            }
+        }
+    }
+
+    private fun saveUserData() : List<String>{
+        var data = listOf<String>()
+        binding.BtnSingUpRegister.setOnClickListener {
+            val username : String = binding.EdtSingUpUsername.text.toString()
+            val password : String = binding.EdtSingUpPassword.text.toString()
+            data = listOf(username, password)
+        }
+        return data
+    }
+
 }
--- app/src/main/java/com/example/fragmentstudy/changeFrag/MainActivity.kt
+++ app/src/main/java/com/example/fragmentstudy/changeFrag/MainActivity.kt
@@ -10,6 +10,7 @@ import com.example.fragmentstudy.databinding.ActivityMainBinding
 class MainActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
